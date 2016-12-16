f932c0428e467ccfbdd36b702a3a78c62f4a2ead
esandine <esandine@stuy.edu>
2016-12-16 12:36:17 -0500

    sdhfsdfhsdhsdhfhsd I didnt get into yale :(

--- intro.js
+++ intro.js
@@ -1,6 +1,20 @@
 var addList = function(e){
-    console.log('hello');
-    document.appendChild(document.getElementsByTagName('<li>')[0]);
+    var ol = document.getElementsByTagName('ol')[0];
+    var li = document.createElement('li');
+    li.innerHTML = "Some text"
+    ol.appendChild(li);
 };
 
-var event = addEventListener('click', addList);
+var removeList = function(index){
+    var ol = document.getElementsByTagName('ol')[0];
+    ol.remove(index);
+    console.log('hello');
+}
+
+var theButton = document.getElementById('b');
+theButton.addEventListener('click', addList);
+var li = document.getElementsByTagName('li')[0];
+var index = 0;
+while index < 8:
+
+li.addEventListener("click",removeList(index);
