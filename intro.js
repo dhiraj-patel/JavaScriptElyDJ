ae457c3af9204a1ca6c12b9cf785771c43d4b731
esandine <esandine@stuy.edu>
2016-12-16 23:15:24 -0500

    Removing elements works

--- intro.js
+++ intro.js
@@ -3,19 +3,25 @@ var addList = function(e){
     var ol = document.getElementsByTagName('ol')[0];
     var li = document.createElement('li');
     li.innerHTML = "Some text"
+    li.addEventListener('click', removeList);
     ol.appendChild(li);
 };
 
 //removeList removes an element from the list
-var removeList = function(index){
-    this.remove(index);
-    console.log('hello');
+var removeList = function(e){
+    this.remove();
 };
 
 var theButton = document.getElementById('b');
 theButton.addEventListener('click', addList);
-li = document.getElementsByTagName('li')[0];
-li.addEventListener('click', removeList(0));
+var list = document.getElementsByTagName('li');
+var i = 0;
+var li;
+while(i<list.length){
+    li = document.getElementsByTagName('li')[i];
+    li.addEventListener('click', removeList);
+    i++;
+}
 /*while(index<ol.length){
     li = ol[index];
     console.log('hello');
