e4675840ca9746aec72e12dd080f0fa1be36cd8e
esandine <esandine@stuy.edu>
2016-12-16 23:35:28 -0500

    fibing around

--- intro.js
+++ intro.js
@@ -7,9 +7,27 @@ var addList = function(e){
     ol.appendChild(li);
 };
 
+var addList = function(e){
+    var ol = document.getElementsByTagName('ol')[0];
+    var li = document.createElement('li');
+    li.innerHTML = "Some text"
+    li.addEventListener('click', removeList);
+    ol.appendChild(li);
+};
+
 //removeList removes an element from the list
 var removeList = function(e){
     this.remove();
+};
+
+var fib = function(n){
+    if(n==0){
+	return 0;
+    }else if(n==1){
+	return 1;
+    }else{
+	return fib(n-1)+fib(n-2);
+    }
 };
 
 var theButton = document.getElementById('b');
@@ -22,3 +40,6 @@ while(i<list.length){
     li.addEventListener('click', removeList);
     i++;
 }
+
+console.log(fib(15))
+//fib returns the next fib element
